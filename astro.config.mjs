64cd361c916a5179f7578ac37d0b756cba1481d2
Agustin Villar <jaguvillar@gmail.com>
2025-09-16 18:37:10 -0300

    feat: Implement responsive header with mobile menu and WhatsApp CTA
    - Added new images for sponsors (Prince and Quento).
    - Refactored Header component to include mobile menu functionality with hamburger and close icons.
    - Created MobileMenu component for mobile navigation.
    - Added WhatsApp CTA button for quick contact.
    - Introduced Footer component to display sponsors and partnerships.
    - Updated global styles for improved design consistency.
    - Enhanced section layouts across various views (About, Canchas, Clases, Lugar, Torneos) for better responsiveness.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    host: true, // o "0.0.0.0"
+    port: 4321, // opcional, si querés forzar un puerto
+  },
+   devToolbar: {
+    enabled: false
+  },
   vite: {
     resolve: {
       alias: {
